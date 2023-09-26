53ac55cb688a2420afbb2a2e24c79008b30bbea7
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-27 01:27:44 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -41,9 +41,8 @@ void bubble_sort(int *array, size_t size)
         {
             if (array[i] > array[i + 1])
             {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
+                int_swap(array +i, array + i + 1;
+                print_array(array, size);
                 swapped = true;
             }
         }
