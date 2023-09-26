66ad4b6d338303e973c7943e20238305c9c0212f
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-26 23:29:14 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,39 @@
 #include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The number of elements in the array.
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+    int aux;
+    size_t i, j, pos;
+    
+    aux = 0;
+    i = 0;
+    j = 0;
+    pos = 0;
+
+    if (array == NULL || size == 0)
+        return;
+
+    for (; i < size - 1; i++)
+    {
+        pos = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[pos])
+                pos = j;
+        }
+        if (pos != i)
+        {
+            aux = array[i];
+            array[i] = array[pos];
+            array[pos] = aux;
+            print_array(array, size);
+        }
+    }
+}
