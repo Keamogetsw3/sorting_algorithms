638333582fa51001333929f627cc07e2e342f75c
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-26 14:02:11 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,3 +6,45 @@
  * @int_2: The second integer to swap.
  *
  */
+void int_swap(int *int_1, int *int_2)
+{
+    int temp;
+    
+    temp = *int_1;
+    *int_1 = *int_2;
+    *int_2 = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the Bubble sort algorit.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ * 
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int swapped;
+    size_t i;
+    size_t j;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int_swap(&array[j], &array[j + 1]);
+                swapped = 1;
+            }
+        }
+
+        print_array(array, size);
+
+        if (swapped == 0)
+        {
+            break;
+        }
+    }
+}
+}
