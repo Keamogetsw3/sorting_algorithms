52f2ae1cd6bc957c855e7463876aabc1a7a6b66d
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-27 01:58:44 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,24 +5,15 @@
  *                  using the selection sort algorithm.
  * @array: The array to be sorted.
  * @size: The number of elements in the array.
- *
  */
 void selection_sort(int *array, size_t size)
 {
-    int aux;
-    size_t i,
-    size_t j,
-    size_t pos;
-    
-    aux = 0;
-    i = 0;
-    j = 0;
-    pos = 0;
+    size_t i, j, pos;
 
     if (array == NULL || size == 0)
         return;
 
-    for (; i < size - 1; i++)
+    for (i = 0; i < size - 1; i++)
     {
         pos = i;
         for (j = i + 1; j < size; j++)
@@ -32,9 +23,9 @@ void selection_sort(int *array, size_t size)
         }
         if (pos != i)
         {
-            aux = array[i];
+            int temp = array[i];
             array[i] = array[pos];
-            array[pos] = aux;
+            array[pos] = temp;
             print_array(array, size);
         }
     }
