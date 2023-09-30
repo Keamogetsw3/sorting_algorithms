3773b9c53dbc3c61b1889720e81a80987a442685
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-10-01 01:46:32 +0200

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,1 +1,21 @@
-# include "sort.h"
+# include "deck.h"
+
+/**
+ * _strncmp - Compares two strings.
+ * @str1: The first string to be compared.
+ * @str2: The second string to be compared.
+ *
+ * Return: + if str1 > str2, 0 if str1 == str2, - if str1 < str2
+ */
+int _strcmp(const char *str1, const char *str2)
+{
+	while (*str1 && *str2 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+	}
+
+	if (*str1 != *str2)
+		return (*str1 - *str2);
+	return (0);
+}
