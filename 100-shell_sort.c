d7f034f34f04c2535e82e4cf3c5e4ee966cbb5c0
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-10-01 04:03:23 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,1 +1,35 @@
-# include "sort.h"
+oid shell_sort(int *array, size_t size)
+{
+    size_t gap;
+    size_t inner
+    size_t outer;
+    int current_element;
+
+    if (!array || size < 2)
+        return;
+
+    gap = 1;
+    while (gap < size / 3)
+    {
+        gap = gap * 3 + 1;
+    }
+
+    while (gap > 0)
+    {
+        for (outer = gap; outer < size; outer++)
+        {
+            current_element = array[outer];
+            inner = outer;
+
+            while (inner > gap - 1 && array[inner - gap] >= current_element)
+            {
+                array[inner] = array[inner - gap];
+                inner = inner - gap;
+            }
+            array[inner] = current_element;
+        }
+
+        gap = (gap - 1) / 3;
+        print_array(array, size);
+    }
+}
